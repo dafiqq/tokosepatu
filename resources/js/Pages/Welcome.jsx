ea0f45ad7a48a3763f6d936b633329a8b9eb5f4f
unknown <kasipahammm@gmail.com>
2024-09-26 22:17:14 +0700

    first commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,62 @@
+import { Head, Link, router } from '@inertiajs/react';
+import Navbar from '@/Components/Navbar';
+
+export default function Welcome({ auth, laravelVersion, phpVersion }) {
+    const handleImageError = () => {
+        document
+            .getElementById('screenshot-container')
+            ?.classList.add('!hidden');
+        document.getElementById('docs-card')?.classList.add('!row-span-1');
+        document
+            .getElementById('docs-card-content')
+            ?.classList.add('!flex-row');
+        document.getElementById('background')?.classList.add('!hidden');
+    };
+
+    return (
+        <>
+            <Head title="Welcome" />
+            <div>
+                <Navbar/>
+            </div>
+            <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
+
+
+
+                        <section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply min-h-screen">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Moro Seneng Jaya</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Menyediakan Sepatu Brand Favorit Anda.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <button onClick={()=>{
+                router.get(route('catalog.index'))
+            }} class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Cari Barang 
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </button>
+        </div>
+    </div>
+    <header className="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                            <nav className="-mx-3 flex flex-1 justify-end">
+                                {auth.user ? (
+                                    <Link
+                                        href={route('dashboard')}
+                                        className="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
+                                        Dashboard
+                                    </Link>
+                                ) : (
+                                    <>
+
+                                    </>
+                                )}
+                            </nav>
+                        </header>
+</section>
+
+                    </div>
+        </>
+    );
+}
